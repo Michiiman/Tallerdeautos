158765bf6a8432f5096363281ca0afb0726e9609
Juan Camilo Robayo Torres <68972889+Michiiman@users.noreply.github.com>
2023-08-05 05:22:47 -0500

    Primera subida de archivos

---
+++ Taller/Program.cs
@@ -0,0 +1,10 @@
+﻿using Taller.View;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Menu menu = new Menu();
+        menu.menu();
+    }
+}
---
+++ Taller/View/MenuPrin.cs
@@ -0,0 +1,19 @@
+namespace Taller.View;
+
+    public class Menu
+    {
+
+            public void menu()
+            {
+                Console.WriteLine("*************************");
+                Console.WriteLine("*********Taller**********");
+                Console.WriteLine("1.Registar Cliente.");
+                Console.WriteLine("2.Registar Vehiculo.");
+                Console.WriteLine("3.Registar Empleado.");
+                Console.WriteLine("4.Generar Orden.");
+                Console.WriteLine("5.Generar Factura.");
+                Console.WriteLine("6.Salir.");
+                Console.WriteLine("-------------------------");
+            }
+
+    }
